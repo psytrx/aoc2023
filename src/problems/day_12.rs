658548500950af334ae76e3b40f546e133489022
psytrx <fj.dumont@gmail.com>
2023-12-12 13:58:58 +0100

    solve example part 2

--- src/problems/day_12.rs
+++ src/problems/day_12.rs
@@ -1,58 +1,34 @@
 pub fn part_one(input: &str) -> anyhow::Result<String> {
-    Ok(parse_input(input)?
+    let sum = parse_input(input)?
         .iter()
-        .map(|r| {
-            let chars = r.pattern.chars().collect::<Vec<_>>();
-            arrangements(&chars, &r.groups, 0)
-        })
-        .sum::<i32>()
-        .to_string())
+        .map(|r| arrangements(&r.pattern, &r.groups))
+        .collect::<anyhow::Result<Vec<usize>>>()?
+        .iter()
+        .sum::<usize>();
+    assert_eq!(sum, 21);
+    Ok(sum.to_string())
 }
 
 pub fn part_two(input: &str) -> anyhow::Result<String> {
-    Ok("".to_string())
+    let sum = parse_input(input)?
+        .into_iter()
+        .map(|r| {
+            let chars = std::iter::repeat(r.pattern)
+                .take(5)
+                .collect::<Vec<_>>()
+                .join("?");
+            let groups = r.groups.repeat(5);
+            arrangements(&chars, &groups)
+        })
+        .collect::<anyhow::Result<Vec<usize>>>()?
+        .iter()
+        .sum::<usize>();
+    assert_eq!(sum, 525152);
+    Ok(sum.to_string())
 }
 
-fn arrangements(pattern: &[char], groups: &[i32], group_size: i32) -> i32 {
-    match (pattern, groups) {
-        ([], []) => {
-            if group_size == 0 {
-                1
-            } else {
-                0
-            }
-        }
-        ([], [expected, rest_groups @ ..]) => {
-            if &group_size == expected {
-                arrangements(pattern, rest_groups, 0)
-            } else {
-                0
-            }
-        }
-        (['.', rest_pat @ ..], []) => {
-            if group_size == 0 {
-                arrangements(rest_pat, groups, 0)
-            } else {
-                0
-            }
-        }
-        (['.', rest_pat @ ..], [expected, rest_groups @ ..]) => {
-            if group_size == 0 {
-                arrangements(rest_pat, groups, 0)
-            } else if &group_size == expected {
-                arrangements(rest_pat, rest_groups, 0)
-            } else {
-                0
-            }
-        }
-        (['?', rest_pat @ ..], _) => {
-            arrangements(&[&['.'], rest_pat].concat(), groups, group_size)
-                + arrangements(&[&['#'], rest_pat].concat(), groups, group_size)
-        }
-        (['#', rest_pat @ ..], _) => arrangements(rest_pat, groups, group_size + 1),
-
-        _ => unreachable!("unreachable pattern: {:?}, groups: {:?}", pattern, groups),
-    }
+fn arrangements(pattern: &str, groups: &[usize]) -> anyhow::Result<usize> {
+    Ok(0)
 }
 
 fn parse_input(input: &str) -> anyhow::Result<Vec<Record>> {
@@ -70,10 +46,10 @@ fn parse_line(line: &str) -> anyhow::Result<Record> {
     let group_counts = group_counts
         .split(',')
         .map(|s| {
-            s.parse::<i32>()
+            s.parse::<usize>()
                 .map_err(|e| anyhow::anyhow!("Failed to parse grou counts: {}", e))
         })
-        .collect::<anyhow::Result<Vec<i32>>>()?;
+        .collect::<anyhow::Result<Vec<usize>>>()?;
 
     Ok(Record {
         pattern: pattern.to_string(),
@@ -84,5 +60,5 @@ fn parse_line(line: &str) -> anyhow::Result<Record> {
 #[derive(Debug)]
 struct Record {
     pattern: String,
-    groups: Vec<i32>,
+    groups: Vec<usize>,
 }
