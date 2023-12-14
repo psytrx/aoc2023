5b9e9ef85b0e563326b03e39ec071265a4900213
psytrx <fj.dumont@gmail.com>
2023-12-14 13:36:27 +0100

    solve part 2

--- src/problems/day_14.rs
+++ src/problems/day_14.rs
@@ -1,15 +1,49 @@
 pub fn part_one(input: &str) -> anyhow::Result<String> {
-    Ok(load(&rotate_dish_cw(&slide_dish_west(&rotate_dish_ccw(
-        &parse_input(input),
-    ))))
-    .to_string())
+    Ok(
+        north_beam_load(&rotate_dish_cw(&slide_dish_west(&rotate_dish_ccw(
+            &parse_input(input),
+        ))))
+        .to_string(),
+    )
 }
 
 pub fn part_two(input: &str) -> anyhow::Result<String> {
-    Ok("foo".to_string())
+    let mut dish = parse_input(input);
+
+    let mut hashes = hashbrown::HashSet::with_capacity(1024);
+    let mut loads = Vec::with_capacity(1024);
+    let mut cycle_start = None;
+
+    let test_cycles = 1_000_000_000;
+
+    for i in 0..test_cycles {
+        let slided_north = slide_dish_west(&rotate_dish_ccw(&dish));
+        let slided_west = slide_dish_west(&rotate_dish_cw(&slided_north));
+        let slided_south = slide_dish_west(&rotate_dish_cw(&slided_west));
+        let slided_east = slide_dish_west(&rotate_dish_cw(&slided_south));
+        dish = rotate_dish_cw(&rotate_dish_cw(&slided_east));
+
+        if !hashes.insert(dish.clone()) {
+            if cycle_start.is_none() {
+                cycle_start = Some(i);
+                hashes.clear();
+                hashes.insert(dish.clone());
+            } else {
+                break;
+            }
+        }
+
+        if cycle_start.is_some() {
+            loads.push(north_beam_load(&dish));
+        }
+    }
+
+    let cycle_start = cycle_start.ok_or_else(|| anyhow::anyhow!("Failed to find cycle start"))?;
+    let cycle_index = (test_cycles - cycle_start) % loads.len() - 1;
+    Ok(loads[cycle_index].to_string())
 }
 
-fn load(dish: &[String]) -> usize {
+fn north_beam_load(dish: &[String]) -> usize {
     dish.iter()
         .enumerate()
         .map(|(i, row)| {
@@ -27,20 +61,23 @@ fn rotate_dish_ccw(dish: &[String]) -> Vec<String> {
 fn rotate_dish_cw(dish: &[String]) -> Vec<String> {
     let mut rotated = Vec::with_capacity(dish[0].len());
     for i in 0..dish[0].len() {
-        let mut row = String::with_capacity(dish.len());
-        for j in (0..dish.len()).rev() {
-            row.push(dish[j].chars().nth(i).unwrap());
+        let mut row = Vec::with_capacity(dish.len());
+        for string in dish.iter().rev() {
+            row.push(string.as_bytes()[i] as char);
         }
-        rotated.push(row);
+        rotated.push(row.into_iter().collect());
     }
     rotated
 }
 
 fn slide_dish_west(dish: &[String]) -> Vec<String> {
-    dish.iter().map(|row| slide_row_west(row)).collect()
+    dish.iter()
+        .map(|row| slide_row_west(row.to_string()))
+        .collect()
 }
 
-fn slide_row_west(row: &str) -> String {
+#[memoize::memoize]
+fn slide_row_west(row: String) -> String {
     let mut slided = row.to_string();
     while slided.contains(".O") {
         slided = slided.replace(".O", "O.");
