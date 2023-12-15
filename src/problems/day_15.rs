fa4545aec99d94ea35f9c26f0d78bc1bf9bb81ef
psytrx <fj.dumont@gmail.com>
2023-12-15 09:34:22 +0100

    solve day 15 part 1

--- src/problems/day_15.rs
+++ src/problems/day_15.rs
@@ -1,7 +1,38 @@
-pub fn part_one(_input: &str) -> anyhow::Result<String> {
+pub fn part_one(input: &str) -> anyhow::Result<String> {
+    Ok(input.trim().split(',').map(hash).sum::<u64>().to_string())
+}
+
+pub fn part_two(input: &str) -> anyhow::Result<String> {
+    let operations = input
+        .trim()
+        .split(',')
+        .map(|operation| {
+            if let Some(label) = operation.strip_suffix('-') {
+                Ok(Operation::Remove(label.to_string()))
+            } else if let Some((label, focal_length)) = operation.split_once('=') {
+                let focal_length = focal_length.parse::<usize>()?;
+                Ok(Operation::Move(label.to_string(), focal_length))
+            } else {
+                unreachable!()
+            }
+        })
+        .collect::<anyhow::Result<Vec<Operation>>>()?;
+    log::trace!("{:?}", operations);
     Ok("not implemented".to_string())
 }
 
-pub fn part_two(_input: &str) -> anyhow::Result<String> {
-    Ok("not implemented".to_string())
+fn hash(step: &str) -> u64 {
+    let mut hash = 0_u64;
+    for &b in step.as_bytes().iter() {
+        hash += b as u64;
+        hash *= 17;
+        hash %= 256;
+    }
+    hash
+}
+
+#[derive(Debug)]
+enum Operation {
+    Remove(String),
+    Move(String, usize),
 }
