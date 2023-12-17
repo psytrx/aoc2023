e3843fa4d9428f4dc6e52042d5eda7df8ff5262b
psytrx <fj.dumont@gmail.com>
2023-12-17 16:05:18 +0100

    solve part 1

--- src/problems/day_16.rs
+++ src/problems/day_16.rs
@@ -1,58 +1,34 @@
 pub fn part_one(input: &str) -> anyhow::Result<String> {
-    let mut contraption = parse_input(input);
-
-    trace_beams(
-        vec![Beam {
+    let contraption = parse_input(input);
+    let energy = compute_energy(
+        &Beam {
             position: (0, 0),
             direction: Direction::Right,
-        }],
-        &mut contraption,
+        },
+        contraption,
     );
+    Ok(energy.to_string())
+}
 
-    let count = contraption
+fn compute_energy(beam: &Beam, contraption: Vec<Vec<Tile>>) -> usize {
+    let beams = vec![beam.clone()];
+    let mut contraption = contraption.clone();
+
+    trace_beams(&beams, &mut contraption);
+
+    contraption
         .iter()
         .flat_map(|row| row.iter())
         .filter(|tile| !tile.beams.is_empty())
-        .count();
-    log::trace!("count: {}", count);
-
-    let display = contraption
-        .iter()
-        .map(|row| {
-            row.iter()
-                .map(|tile| match tile.kind {
-                    '|' => '|',
-                    '-' => '-',
-                    '/' => '/',
-                    '\\' => '\\',
-                    '.' => match tile.beams.len() {
-                        0 => '.',
-                        1 => {
-                            let beam = tile.beams.iter().next().unwrap();
-                            match beam.direction {
-                                Direction::Up => '^',
-                                Direction::Down => 'v',
-                                Direction::Left => '<',
-                                Direction::Right => '>',
-                            }
-                        }
-                        n => std::char::from_digit(n as u32, 10).unwrap(),
-                    },
-                    _ => unreachable!(),
-                })
-                .collect::<String>()
-        })
-        .collect::<Vec<_>>()
-        .join("\n");
-    log::trace!("display:\n{}", display);
-
-    Ok("not implemented".to_string())
+        .count()
 }
 
-fn trace_beams(mut beams: Vec<Beam>, contraption: &mut Vec<Vec<Tile>>) {
+fn trace_beams(beams: &[Beam], contraption: &mut [Vec<Tile>]) {
+    let mut beams = beams.to_vec().clone();
+
     while let Some(beam) = beams.pop() {
         let (x, y) = beam.position;
-        if x < 0 || y < 0 || x >= contraption[0].len() as i32 || y >= contraption.len() as i32 {
+        if x < 0 || y < 0 || y >= contraption.len() as i32 || x >= contraption[0].len() as i32 {
             continue;
         }
 
@@ -69,10 +45,7 @@ fn trace_beams(mut beams: Vec<Beam>, contraption: &mut Vec<Vec<Tile>>) {
                     Direction::Left => (x - 1, y),
                     Direction::Right => (x + 1, y),
                 };
-                beams.push(Beam {
-                    position,
-                    direction: beam.direction,
-                });
+                beams.push(Beam { position, ..beam });
             }
             '-' => match beam.direction {
                 Direction::Left | Direction::Right => {
@@ -82,23 +55,15 @@ fn trace_beams(mut beams: Vec<Beam>, contraption: &mut Vec<Vec<Tile>>) {
                         _ => unreachable!(),
                     };
                     let position = (new_x, y);
-                    beams.push(Beam {
-                        position,
-                        direction: beam.direction,
-                    })
+                    beams.push(Beam { position, ..beam })
                 }
                 Direction::Up | Direction::Down => {
-                    let new_y = match beam.direction {
-                        Direction::Up => y - 1,
-                        Direction::Down => y + 1,
-                        _ => unreachable!(),
-                    };
                     beams.push(Beam {
-                        position: (x - 1, new_y),
+                        position: (x - 1, y),
                         direction: Direction::Left,
                     });
                     beams.push(Beam {
-                        position: (x + 1, new_y),
+                        position: (x + 1, y),
                         direction: Direction::Right,
                     });
                 }
@@ -112,21 +77,16 @@ fn trace_beams(mut beams: Vec<Beam>, contraption: &mut Vec<Vec<Tile>>) {
                     };
                     beams.push(Beam {
                         position: (x, new_y),
-                        direction: beam.direction,
+                        ..beam
                     })
                 }
                 Direction::Left | Direction::Right => {
-                    let new_x = match beam.direction {
-                        Direction::Left => x - 1,
-                        Direction::Right => x + 1,
-                        _ => unreachable!(),
-                    };
                     beams.push(Beam {
-                        position: (new_x, y - 1),
+                        position: (x, y - 1),
                         direction: Direction::Up,
                     });
                     beams.push(Beam {
-                        position: (new_x, y + 1),
+                        position: (x, y + 1),
                         direction: Direction::Down,
                     });
                 }
@@ -190,6 +150,7 @@ fn parse_input(input: &str) -> Vec<Vec<Tile>> {
         .collect()
 }
 
+#[derive(Clone)]
 struct Tile {
     kind: char,
     beams: std::collections::HashSet<Beam>,
