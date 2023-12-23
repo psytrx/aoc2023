05665248f638f854da68352f250df007c975e49b
psytrx <fj.dumont@gmail.com>
2023-12-23 15:03:22 +0100

    solve day 23 part 1

--- src/problems/day_23.rs
+++ src/problems/day_23.rs
@@ -1,73 +1,123 @@
 pub fn part_one(input: &str) -> anyhow::Result<String> {
-    let map = parse_input(input)?;
-    let cost = find_longest_path(
-        &map,
-        (1, 0),
-        (21, 22),
-        0,
-        0,
-        vec![vec![false; map[0].len()]; map.len()],
-    );
-    Ok(cost.to_string())
+    Ok(solve(input, false)?.to_string())
 }
 
-fn find_longest_path(
-    map: &[Vec<char>],
-    cell: (usize, usize),
-    end: (usize, usize),
-    mut path_len: usize,
+pub fn part_two(input: &str) -> anyhow::Result<String> {
+    Ok(solve(input, true)?.to_string())
+}
+
+fn solve(input: &str, ignore_slope: bool) -> anyhow::Result<usize> {
+    let map = parse_input(input)?;
+
+    let g = build_graph(&map, ignore_slope);
+    let mut visited = vec![false; g.nodes.len()];
+    let res = find_longest_path_tree(&g, 1, g.nodes.len() - 2, 0, 0, &mut visited);
+
+    Ok(res)
+}
+
+fn find_longest_path_tree(
+    g: &Graph,
+    node_idx: usize,
+    end_idx: usize,
+    path_len: usize,
     max_path_len: usize,
-    mut visited: Vec<Vec<bool>>,
+    visited: &mut Vec<bool>,
 ) -> usize {
-    if cell == end {
+    if node_idx == end_idx {
         return path_len.max(max_path_len);
     }
 
-    let (x, y) = cell;
-    visited[y][x] = true;
-    path_len += 1;
-
-    let moves = match map[y][x] {
-        '#' => vec![],
-        '.' => vec![(1, 0), (0, 1), (-1, 0), (0, -1)],
-        '>' => vec![(1, 0)],
-        'v' => vec![(0, 1)],
-        '<' => vec![(-1, 0)],
-        '^' => vec![(0, -1)],
-        _ => unreachable!(),
-    };
-
+    visited[node_idx] = true;
     let mut new_max_path_len = max_path_len;
-    for (dx, dy) in moves {
-        let x = x as i32 + dx;
-        let y = y as i32 + dy;
-        if y < 0 || y >= map.len() as i32 || x < 0 || x >= map[0].len() as i32 {
+
+    let node = &g.nodes[node_idx];
+    for &neighbor_idx in node.neighbors.iter() {
+        if visited[neighbor_idx] {
             continue;
         }
 
-        let (x, y) = (x as usize, y as usize);
-        if !visited[y][x] {
-            new_max_path_len = new_max_path_len.max(find_longest_path(
-                map,
-                (x, y),
-                end,
-                path_len,
-                new_max_path_len,
-                visited.clone(),
-            ));
-        }
+        new_max_path_len = new_max_path_len.max(find_longest_path_tree(
+            g,
+            neighbor_idx,
+            end_idx,
+            path_len + 1,
+            new_max_path_len,
+            visited,
+        ));
     }
 
+    visited[node_idx] = false;
     new_max_path_len
 }
 
-pub fn part_two(_input: &str) -> anyhow::Result<String> {
-    Ok("not implemented".to_string())
+fn build_graph(map: &[Vec<u8>], ignore_slope: bool) -> Graph {
+    let mut nodes = map
+        .iter()
+        .flat_map(|row| {
+            row.iter()
+                .map(|_| Node { neighbors: vec![] })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    for y in 0..map.len() {
+        for x in 0..map[y].len() {
+            let cell = map[y][x];
+            if cell == b'#' {
+                continue;
+            }
+
+            let moves = if ignore_slope {
+                vec![(1, 0), (0, 1), (-1, 0), (0, -1)]
+            } else {
+                match cell {
+                    b'.' => vec![(1, 0), (0, 1), (-1, 0), (0, -1)],
+                    b'>' => vec![(1, 0)],
+                    b'v' => vec![(0, 1)],
+                    b'<' => vec![(-1, 0)],
+                    b'^' => vec![(0, -1)],
+                    _ => unreachable!(),
+                }
+            };
+
+            let curr_idx = y * map[0].len() + x;
+
+            nodes[curr_idx].neighbors = moves
+                .iter()
+                .filter_map(|(dx, dy)| {
+                    let (n_x, n_y) = (x as i32 + dx, y as i32 + dy);
+                    if n_y < 0 || n_y >= map.len() as i32 || n_x < 0 || n_x >= map[0].len() as i32 {
+                        return None;
+                    }
+
+                    let n_cell = map[n_y as usize][n_x as usize];
+                    if n_cell == b'#' {
+                        return None;
+                    }
+
+                    let n_idx = n_y as usize * map[0].len() + n_x as usize;
+                    Some(n_idx)
+                })
+                .collect::<Vec<_>>();
+        }
+    }
+
+    Graph { nodes }
 }
 
-fn parse_input(input: &str) -> anyhow::Result<Vec<Vec<char>>> {
+struct Graph {
+    nodes: Vec<Node>,
+}
+
+#[derive(Clone, Debug)]
+struct Node {
+    neighbors: Vec<usize>,
+}
+
+fn parse_input(input: &str) -> anyhow::Result<Vec<Vec<u8>>> {
     Ok(input
         .lines()
-        .map(|line| line.chars().collect::<Vec<_>>().to_owned())
+        .map(|line| line.as_bytes().to_owned())
         .collect())
 }
