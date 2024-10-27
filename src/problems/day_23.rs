95e2c0d1c1953ab09c4e31a0ea26aae763cab126
psytrx <fj.dumont@gmail.com>
2024-10-27 19:04:27 +0100

    re-enable day 2e part 2

--- src/problems/day_23.rs
+++ src/problems/day_23.rs
@@ -2,9 +2,9 @@ pub fn part_one(input: &str) -> anyhow::Result<String> {
     Ok(solve(input, false)?.to_string())
 }
 
-pub fn part_two(_input: &str) -> anyhow::Result<String> {
-    Ok("skipped".to_string())
-    // Ok(solve(input, true)?.to_string())
+pub fn part_two(input: &str) -> anyhow::Result<String> {
+    // Ok("skipped".to_string())
+    Ok(solve(input, true)?.to_string())
 }
 
 fn solve(input: &str, ignore_slope: bool) -> anyhow::Result<usize> {
